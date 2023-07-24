f4851858468a8cd28d6a7c86ec38516998e9fc72
Mohammad Akram <mohammadakram1708@gmail.com>
2023-07-24 20:06:07 +0530

    New design added, login and signup API call added

--- client/src/side-panel.js
+++ client/src/side-panel.js
@@ -14,6 +14,7 @@ iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
+iframe.style.borderLeft = "3px solid #4e81e9";
 // in the window.location.hash replace the first # character with /
 const hashConverter = window.location.hash.replace("#", "#/");
 iframe.src = chrome.runtime.getURL("index.html"+hashConverter)
